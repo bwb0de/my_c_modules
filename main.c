07ec99b61a74e422fd2dd73a57188afcbd86c78d
Daniel Cruz <danielc@unb.br>
2024-09-20 11:32:53 -0300

    ...

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
-#include "cycling.h"
-#include "array.h"
+#include <stdio.h>
 
 int main() {
-    _teste_array_par_de_inteiros();
+    //codigo para identificar pares pr[oximos em um array a partir dos indices. Primeira linha.. Partir para o merge...]
+    int n = 677;
+    int pairs;
+    if ( n % 2 == 1 ) {
+        pairs = (n - 1) / 2;
+
+    }
+    printf("%d\n", 15 % 2);
+    printf("%d\n", pairs);
+
+    int a = 0;
+    int b = 1;
+
+    while (pairs > 0 ) {
+        printf("(%d,%d); ", a, b);
+        a += 2; b += 2;
+        pairs--;
+    }
+    
+
     
     return 0;
 }
