84644f0635ee32b7ac2dd55de369e3059776d69d
Daniel Cruz <danielc@unb.br>
2025-06-07 10:00:06 -0300

    Restringindo branch ao siur...

--- config.lua
+++
@@ -1,8 +0,0 @@
-player = {
-    name = "Daniel Birigua",
-    vida = 100,
-    mana = 50,
-    posicao = { x = 10, y = 20 },
-    ativo = true
-}
-
--- lua_recive_C.lua
+++
@@ -1,8 +0,0 @@
-print("Nome do jogador:", nome_do_jogador)
-print("Nível:", nivel_do_jogador)
-
-if nivel_do_jogador > 30 then
-    print("O jogador é experiente.")
-else
-    print("O jogador ainda está aprendendo.")
-end
