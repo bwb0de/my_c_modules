a18070f9d335971681c2fe3178a86c5720056c81
Daniel Cruz <danielc@unb.br>
2025-06-07 10:10:29 -0300

    Eliminando estrutura includes antiga

--- lib/mathext.c
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-
-
-
-double ln(double n) {
-    return log(n);
-}
--- testes/teste_geometria.c
+++
@@ -1,9 +0,0 @@
-/*
-#include "geometria.h"
-
-int main() {
-    _teste_ponto_2D();
-    _teste_ponto_3D();
-    return 0;
-}
-*/
