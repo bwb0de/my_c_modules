394239634d6db0ae430f6ef4a58d78cac9700a58
Daniel Cruz <danielc@unb.br>
2025-06-07 11:30:47 -0300

    Incluindo queue2 de Termux

---
+++ includes/queue2.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include "heap.h"
+
+typedef struct Fila {
+    uint32_t idx;
+    heap_element_t *q;
+} queue_t;
+
+
+queue_t init_queue(uint32_t size) {
+    heap_element_t malloc()
+
+
+}
